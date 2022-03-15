cf491ad935ae8090cea38fdd526157d5a2f2750e
Aravind-GRL <99865728+Aravind-GRL@users.noreply.github.com>
2022-03-15 17:11:25 +0530

    cloned into another repo

---
+++ purchase_management/module/views.py
@@ -0,0 +1,200 @@
+from django.shortcuts import render,redirect
+from module.models import purchase,project,purchase_request,user_info,user_type
+from module.forms import ProjectForm,PurchaseForm,RequestForm
+from datetime import date
+from django.http import HttpResponse
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
+from django.core.mail import send_mail
+
+
+# Create your views here.
+
+def login(request):
+    if request.method == "POST":
+        name = request.POST.get('uname')
+        password = request.POST.get('pwd')
+        if user_info.objects.filter(uname=name,password=password).exists():
+            check_utype = user_info.objects.filter(uname=name,password=password).values('user_type')
+            user_type = list(check_utype)[0]['user_type']
+            request.session['utype']=user_type
+            request.session['uname'] = name
+            request.session['is_login'] = True
+            if user_type == '1':
+                return redirect("/index")
+            elif user_type == '2':
+                return redirect("/pr_index")
+            else:
+                return redirect("/pu_index")
+        else:
+            messages.error(request,'Invalid Credentials')
+            return redirect("/login")
+    else:    
+        return render(request,'login.html')
+
+def logout(request):
+    del request.session['uname']
+    request.session['is_login'] = False
+    return redirect("/login")
+
+def user_list(request):
+    users = user_info.objects.values()
+    return render(request,'user_list.html',{'user_list':users})
+
+def edit_users(request,id):
+    user_details = user_info.objects.get(uid=id)
+    utype = user_type.objects.all()
+    return render(request,'edit_users.html',{'user':user_details,'utype':utype})
+    
+
+def main(request):
+    if request.method == "POST":  
+        utype = request.POST['utype']
+        if 'Employee' in utype:
+            return redirect('/index')
+        elif 'Project_Manager' in utype:
+            return redirect('/pr_index')
+        else:
+            return redirect('/pu_index')
+    else:
+        return render(request,'main.html')
+
+
+def index(request): 
+    if not request.session['is_login']:
+        messages.error(request,'Please login to perform this action') 
+        return redirect("/login")
+    if request.session['utype'] != '1':
+        messages.error(request,'You don\'t have access for this page') 
+        return redirect("/login")
+    purchase_req = purchase_request.objects.values()
+    project_mgr = project.objects.values() 
+    purchase_mgr = purchase.objects.values() 
+    return render(request,'index.html',{'pur_req':purchase_req,'pj_mgr':project_mgr,'pu_mgr':purchase_mgr})
+    
+
+def add(request):
+    if not request.session['is_login']:
+        messages.error(request,'Please login to perform this action') 
+        return redirect("/login")
+    if request.method == "POST":  
+        name = request.POST['name']
+        team = request.POST['team']
+        req_pro = request.POST['req_pro']
+        purpose = request.POST['purpose']
+        link = request.POST['link']
+        price = request.POST['price']
+        mgr = request.POST['mgr']
+        img = request.FILES['pro_file']
+        fss = FileSystemStorage()
+        file = fss.save(img.name, img)
+        file_url = fss.url(file)
+        insert = purchase_request.objects.create(emp_name=name,team=team,req_product=req_pro,purpose=purpose,product_link=link,
+        price=price,project_manager=str(mgr),purchase_manager='Not Assigned',status='Pending for Approval',request_date=date.today(),image=img)
+        messages.success(request,'Request added successfully')
+        return redirect('/index')
+    else:
+        project_mgr = project.objects.all() 
+        #form = RequestForm()
+    return render(request,'add_req.html',{'manager':project_mgr}) 
+
+
+def project_index(request):  
+    if not request.session['is_login']:
+        messages.error(request,'Please login to perform this action') 
+        return redirect("/login")
+    if request.session['utype'] != '2':
+        messages.error(request,'You don\'t have access for this page') 
+        return redirect("/login") 
+    user_details = user_info.objects.filter(uname=request.session['uname']).values_list('team',flat=True)
+    purchase_req = purchase_request.objects.filter(team__in=user_details).values()
+    return render(request,'project_index.html',{'pur_req':purchase_req})
+
+
+def purchase_index(request):  
+    if not request.session['is_login']:
+        messages.error(request,'Please login to perform this action') 
+        return redirect("/login")
+    if request.session['utype'] != '3':
+        messages.error(request,'You don\'t have access for this page') 
+        return redirect("/login")
+    purchase_req = purchase_request.objects.values() 
+    project_mgr = project.objects.values() 
+    purchase_mgr = purchase.objects.values() 
+    return render(request,'purchase_index.html',{'pur_req':purchase_req,'pj_mgr':project_mgr,'pu_mgr':purchase_mgr})
+
+
+def change_status(request):
+    req_id = request.GET.get('id')
+    status = request.GET.get('status')
+    utype = request.GET.get('utype')
+    if 'project' in utype:
+        if '1' in status:
+            status = 'Approved by project manager'
+            purchase_manager = purchase.objects.values('name').filter(purchase_manager_id=1) 
+            approve_date = date.today() 
+            update = purchase_request.objects.filter(req_id=req_id).update(status=status,approved_date=approve_date,purchase_manager=list(purchase_manager)[0]['name'])
+        elif '2' in status:
+            status = 'Rejected by project manager'
+            purchase_manager = purchase.objects.values('name').filter(purchase_manager_id=0)
+            update = purchase_request.objects.filter(req_id=req_id).update(status=status)
+    elif 'purchase' in utype:
+        if '1' in status:
+            status = 'Order placed'
+            purchase_manager = purchase.objects.values('name').filter(purchase_manager_id=1)
+            purchase_date = date.today() 
+            update = purchase_request.objects.filter(req_id=req_id).update(status=status,approved_date=purchase_date,purchase_date=purchase_date,purchase_manager=list(purchase_manager)[0]['name'])  
+        elif '2' in status:
+            status = 'Rejected by purchase manager'
+            update = purchase_request.objects.filter(req_id=req_id).update(status=status)  
+        elif '3' in status:
+            status = 'Closed'
+            del_date = date.today() 
+            update = purchase_request.objects.filter(req_id=req_id).update(status=status)
+     
+    return HttpResponse('success') 
+
+
+def update_delivery_date(request):
+    req_id = request.GET.get('id')
+    del_date = request.GET.get('del_date')
+    update = purchase_request.objects.filter(req_id=req_id).update(delivery_date=del_date)
+    return HttpResponse('Delivery date updated')
+
+def upload_invoice(request):
+    if request.method == "POST":  
+        inv = request.FILES['invoice']
+        req_id = request.POST['req_id']
+        inv_name = inv.name.replace(inv.name.split('.')[0],req_id)
+        fss = FileSystemStorage()
+        file = fss.save(inv.name.replace(inv.name.split('.')[0],req_id), inv)
+        file_url = fss.url(file)
+        update = purchase_request.objects.filter(req_id=req_id).update(invoice=inv_name)
+        messages.success(request,"Invoice Uploaded")
+        return redirect('/pu_index')
+    else:
+        print('hi')
+        pass
+
+def registration(request):
+    if request.method == "POST":  
+        name = request.POST['name']
+        email = request.POST['email']
+        team = request.POST['team']
+        utype = request.POST['utype']
+        uname = request.POST['uname']
+        password = request.POST['pwd']
+        if utype=='2':
+            pj_manager_insert = project.objects.create(name=name)
+        elif utype=='3':
+            pu_manager_insert = purchase.objects.create(name=name)
+        user_insert = user_info.objects.create(name=name,email=email,team=team,user_type=utype,uname=uname,password=password,created_on=date.today())
+        messages.success(request,'User Created Successfully')
+        return redirect("/pu_index") 
+    else:
+        utype = user_type.objects.all()
+        return render(request,'registration.html',{'utype':utype}) 
+    
+    
+
+
---
+++ purchase_management/purchase_management/urls.py
@@ -0,0 +1,38 @@
+"""purchase_management URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from module import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index',views.index),
+    path('add',views.add),
+    path('edit_users/<int:id>',views.edit_users),
+    path('delete_users/<int:id>',views.edit_users),
+    path('',views.main),
+    path('user_list',views.user_list),
+    path('pr_index',views.project_index),
+    path('pu_index',views.purchase_index),
+    path('change_status',views.change_status),
+    path('update_delivery_date',views.update_delivery_date),
+    path('upload_invoice',views.upload_invoice),
+    path('login',views.login),
+    path('logout',views.logout),
+    path('registration',views.registration)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
